d0c3409227c83fdc4f51c522e0c66af2c18fa973
sibbe23 <sibbe23@gmail.com>
2024-02-19 14:30:14 +0530

    Basics

--- src/App.js
+++ src/App.js
@@ -10,19 +10,33 @@ import Sidebar from './components/Sidebar/Sidebar'; // Import Sidebar component
 function App() {
   const [darkMode, setDarkMode] = useState(false); // State for dark mode
 
+  // Function to determine if sidebar should be displayed
+  const shouldDisplaySidebar = (path) => {
+    // Check if the current path is not "/login" or "/candidate-login"
+    return !['/login', '/candidate-login'].includes(path);
+  };
+
   return (
     <Router>
       <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
         <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} /> {/* Pass down dark mode state */}
-        <Sidebar darkMode={darkMode} /> {/* Pass down dark mode state */}
-
         <Routes>
           <Route path="/login" element={<LoginForm />} />
           <Route path="/candidate-login" element={<CandidateLogin />} />
           {/* Pass down dark mode state to UserDashboard and CandidateDashboard */}
-          <Route path="/user-dashboard" element={<UserDashboard />} />
-          <Route path="/candidate-dashboard" element={<CandidateDashboard />} />
+          <Route
+            path="/user-dashboard"
+            element={<UserDashboard />}
+            shouldDisplaySidebar={shouldDisplaySidebar}
+          />
+          <Route
+            path="/candidate-dashboard"
+            element={<CandidateDashboard />}
+            shouldDisplaySidebar={shouldDisplaySidebar}
+          />
         </Routes>
+        {/* Conditional rendering of Sidebar */}
+        {shouldDisplaySidebar(window.location.pathname) && <Sidebar darkMode={darkMode} />}
       </div>
     </Router>
   );
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Offcanvas } from 'bootstrap';
-import { BsList } from 'react-icons/bs'; // Hamburger icon
+import { BsList, BsFillPersonFill } from 'react-icons/bs'; // Hamburger icon"
+
+import { Collapse, Dropdown } from 'react-bootstrap'; // Importing Collapse component from react-bootstrap
 
 const Sidebar = ({ darkMode }) => {
   useEffect(() => {
@@ -9,7 +11,7 @@ const Sidebar = ({ darkMode }) => {
     const offcanvas = new Offcanvas(offcanvasElement, {
       backdrop: false // Disable backdrop
     });
-    
+
     // Show the offcanvas when component mounts
     offcanvas.show();
 
@@ -26,10 +28,15 @@ const Sidebar = ({ darkMode }) => {
     };
   }, []);
 
-  const [companyOpen, setCompanyOpen] = useState(false);
+  // State management for dropdowns
+  const [activeDropdown, setActiveDropdown] = useState(null);
 
-  const toggleCompany = () => {
-    setCompanyOpen(!companyOpen);
+  const handleDropdownToggle = (dropdownName) => {
+    setActiveDropdown(activeDropdown === dropdownName ? null : dropdownName);
+  };
+
+  const handleLogout = () => {
+    // Implement logout functionality
   };
 
   return (
@@ -38,22 +45,228 @@ const Sidebar = ({ darkMode }) => {
       <button className="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
         <BsList />
       </button>
-      <div className={`offcanvas offcanvas-start ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style={{width: '200px'}}>
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasExampleLabel">Nsnemo</h5>
+      <div className={`offcanvas offcanvas-start ${darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style={{ width: '200px' }}>
+        <div className="offcanvas-header mt-1 mb-0 pb-0">
+          <h5 className="offcanvas-title  fst-italic " id="offcanvasExampleLabel">Nsnemo</h5>
           <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
-          {/* Sidebar Content */}
-          <button type="button" className="btn btn-secondary mb-2 d-block w-100" onClick={toggleCompany}>
-            Company
-          </button>
-          {companyOpen && (
-            <div className="company-buttons">
-              <button type="button" className="btn btn-secondary mb-2 d-block w-100">Add</button>
-              <button type="button" className="btn btn-secondary d-block w-100">List</button>
-            </div>
-          )}
+          {/* Settings */}
+          <div>
+            <p style={{ fontSize: '10px' }} className='mb-1 fst-italic '><li>Settings</li></p>
+            {/* Company Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 company-btn ${activeDropdown === 'company' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('company')}
+            >
+              Company <span className={`float-end ${activeDropdown === 'company' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'company'}>
+              <div className="company-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Vessel Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 vessel-btn ${activeDropdown === 'vessel' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('vessel')}
+            >
+              Vessel <span className={`float-end ${activeDropdown === 'vessel' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'vessel'}>
+              <div className="vessel-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Experience Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 experience-btn ${activeDropdown === 'experience' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('experience')}
+            >
+              Experience <span className={`float-end ${activeDropdown === 'experience' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'experience'}>
+              <div className="experience-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Rank Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 rank-btn ${activeDropdown === 'rank' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('rank')}
+            >
+              Rank <span className={`float-end ${activeDropdown === 'rank' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'rank'}>
+              <div className="rank-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Grade Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 grade-btn ${activeDropdown === 'grade' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('grade')}
+            >
+              Grade <span className={`float-end ${activeDropdown === 'grade' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'grade'}>
+              <div className="grade-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Port Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 port-btn ${activeDropdown === 'port' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('port')}
+            >
+              Port <span className={`float-end ${activeDropdown === 'port' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'port'}>
+              <div className="port-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Port Agent Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 port-agent-btn ${activeDropdown === 'portAgent' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('portAgent')}
+            >
+              Port Agent <span className={`float-end ${activeDropdown === 'portAgent' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'portAgent'}>
+              <div className="port-agent-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Hospital Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 hospital-btn ${activeDropdown === 'hospital' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('hospital')}
+            >
+              Hospital <span className={`float-end ${activeDropdown === 'hospital' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'hospital'}>
+              <div className="hospital-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Document Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 doc-btn ${activeDropdown === 'doc' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('doc')}
+            >
+              Document <span className={`float-end ${activeDropdown === 'doc' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'doc'}>
+              <div className="doc-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Country Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 country-btn ${activeDropdown === 'country' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('country')}
+            >
+              Country <span className={`float-end ${activeDropdown === 'country' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'country'}>
+              <div className="country-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+            {/* Vendor Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 w-100 vendor-btn ${activeDropdown === 'vendor' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('vendor')}
+            >
+              Vendor <span className={`float-end ${activeDropdown === 'vendor' ? 'arrow-up' : 'arrow-down'}`}></span>
+            </button>
+            <Collapse in={activeDropdown === 'vendor'}>
+              <div className="vendor-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">List</button>
+              </div>
+            </Collapse>
+          </div>
+          {/* End of Settings */}
+          {/* Candidate Management */}
+          <div>
+            <p style={{ fontSize: '10px' }} className='mt-3 mb-1 fst-italic '><li>Candidate Management</li></p>
+            {/* Candidate Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2 d-block w-100 ${activeDropdown === 'candidate' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('candidate')}
+            >
+              Candidate
+            </button>
+            <Collapse in={activeDropdown === 'candidate'}>
+              <div className="candidate-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100" >Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 d-block w-100 mb-2">List</button>
+              </div>
+            </Collapse>
+          </div>
+          {/* End of Candidate Management */}
+          {/* User Management */}
+          <div>
+            <p style={{ fontSize: '10px' }} className='mt-3 mb-1 fst-italic '><li>User Management</li></p>
+            {/* User Dropdown */}
+            <button
+              type="button"
+              className={`btn btn-outline-primary text-start border-0 mb-2  d-block w-100 ${activeDropdown === 'user' ? 'active' : ''}`}
+              onClick={() => handleDropdownToggle('user')}
+            >
+              User
+            </button>
+            <Collapse in={activeDropdown === 'user'}>
+              <div className="user-buttons">
+                <button type="button" className="btn btn-outline-primary text-start border-0 mb-2 d-block w-100">Add</button>
+                <button type="button" className="btn btn-outline-primary text-start border-0 d-block w-100 mb-2">List</button>
+              </div>
+            </Collapse>
+          </div>
+          {/* End of User Management */}
+          <div>
+            {/* Other dropdowns go here */}
+            {/* Search Dropdown */}
+            <p style={{ fontSize: '10px' }} className='mt-3 mb-1 fst-italic '><li>Others</li></p>
+            <button type='button' className='btn btn-outline-primary text-start border-0 mb-2 d-block w-100' onClick={() => window.open('https://example.com', '_blank')}>
+              Search
+            </button>
+            {/* Reports Dropdown */}
+            <button type='button' className='btn btn-outline-primary text-start border-0 mb-2 d-block w-100' onClick={() => window.open('https://example.com/reports', '_blank')}>
+              Reports
+            </button>
+            {/* Help Dropdown */}
+            <button type='button' className='btn btn-outline-primary text-start border-0 mb-2 d-block w-100' onClick={() => window.open('https://example.com/help', '_blank')}>
+              Crew Planner
+            </button>
+            {/* Logout Button */}<hr></hr>
+            <button type='button' className='btn btn-outline-danger text-start border-0 mb-2 d-block w-100' onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
         </div>
       </div>
     </>
